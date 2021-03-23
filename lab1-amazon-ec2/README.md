29f67c7f1f4850a5b6098f0bec53cb3feb21d895
15b883 <syavingc.s@gmail.com>
2021-03-23 21:57:51 +0800

    only-one-ec2-instance

--- lab1-amazon-ec2/README.md
+++ lab1-amazon-ec2/README.md
@@ -1,6 +1,17 @@
 
 
-Demo 测试创建EC2实例
+
+# 只能创建1台EC2实例
+
+1、通过默认VPC创建EC2机器
+2、创建eip
+3、创建安全组
+4、导入自定义key
+5、打开ICMP
+
+
+注意！
+
 
 
 usage
@@ -10,4 +21,20 @@ terraform init
 terraform plan 
 terraform apply
 terraform destroy
+```
+
+```
+➜  ~ ssh -i .ssh/15b883 ec2-user@35.73.71.119
+The authenticity of host '35.73.71.119 (35.73.71.119)' can't be established.
+ECDSA key fingerprint is SHA256:jdw9rgV1FKZ2bjrVhNSpEjLy68Wh2tz5Gu7fcP/SJfA.
+Are you sure you want to continue connecting (yes/no/[fingerprint])? yes
+Warning: Permanently added '35.73.71.119' (ECDSA) to the list of known hosts.
+
+       __|  __|_  )
+       _|  (     /   Amazon Linux 2 AMI
+      ___|\___|___|
+
+https://aws.amazon.com/amazon-linux-2/
+[ec2-user@ip-172-31-38-131 ~]$
+[ec2-user@ip-172-31-38-131 ~]$
 ```
--- lab1-amazon-ec2/main.tf
+++ lab1-amazon-ec2/main.tf
@@ -3,13 +3,64 @@ provider "aws" {
   region     = "ap-northeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0bc8ae3ec8e338cbc"  
-  instance_type = "t2.micro"
+resource "aws_key_pair" "example-key" {
+	key_name   = "example-key"
+	public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC6iEfAf/xITItlSJqBNnCaw1BI+yqRPgnDSWUuGWoQJIukWmxinizySPC7ful2H+XyEbCHzkd5nPZobJwlCiF1oKEI2cFNB5ke02R//iGL1svPYj1hSKkrQECdiK2UgzBI9XI3a5ZhkNPj0BYQVOrnx3Jy/98LdP43zRS/mOADyvlGbmg8EI+Wb1TfzgRAl+z0GBPE7xMw9dGY+WlyuuLYS2NrYVPQ8FyHjcBYgB8Gd6KepntkwS/FMpF1ok66mo8G1hh9rwjTyP7AMlsiz1vxN8/PMZDwoN0tL8uuEktRA12m+x2Xp1ln4quEBwLlH5jgtUNNsSeuk3abYYqSHwti1fZv86Wf1XbbIaaJUU5tgjyqQ0ZjPY02p/7pqcZ5uI2Druyv2nX9079uYjhjZOMT+fjBQtLcglVt4gcGIak7sZwuo1nLLszZ1k8H6wTfCUTcSGMxEYzyCh4rDpTfF77PIhK47N33lwL4aH4YEdbc/hlakeF96oybsZ4OMgyclbhYoVq8s24vLWd0vUUmOemDiUm81VkzWfZHkRdFbjR6ngxlkYvacM4OCi5uSaGdztBuO+exsdCAbyB5yrX/DWeZVDUoG7fv99e/wUK6A7se8oJnhzfMYaubu0sCZLOP1xDXhbn7/M8124aacZVcs2+T/sm6jk2Kdq42jaEBqShD5w== root@localhost.localdomain"
+}
 
+resource "aws_instance" "example" {
+  ami                = "ami-0bc8ae3ec8e338cbc"  
+  key_name           = "example-key"
+  instance_type      = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.example.id ]
 
 
   tags = {
     "Name" = "terraform-example"
   }
+}
+
+
+resource "aws_eip" "eip_example" {
+  instance = aws_instance.example.id
+  vpc  =  true
+
+  tags = {
+    "Name" = "example_eip"
+  }
+}
+
+resource "aws_security_group" "example" {
+  # ICMP
+  ingress {
+		from_port   = -1
+		to_port     = -1
+		protocol    = "icmp"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "icmp"
+	}
+
+  
+  # Inbound internet access
+  ingress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "inbound"
+	}
+	  
+	# Outbound internet access
+	egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+    description = "outbound"
+	}
+	
+	tags = {
+		Name = "example_sg"
+	}
+  
 }
