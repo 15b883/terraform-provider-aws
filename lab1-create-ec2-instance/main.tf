31be670caf1cc62c88749d29af09ed1c605ec25c
15b883 <syavingc.s@gmail.com>
2021-03-23 15:22:47 +0800

    create-ec2-instance

---
+++ lab1-create-ec2-instance/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  profile    = "jp"
+  region     = "ap-northeast-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0bc8ae3ec8e338cbc"  
+  instance_type = "t2.micro"
+
+
+
+  tags = {
+    "Name" = "terraform-example"
+  }
+}
